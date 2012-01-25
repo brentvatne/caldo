1b86a041a9615d9c05ee22d872d94ab637b6e02b
Brent Vatne <brentvatne@gmail.com>
2012-01-25 14:06:03 -0800

    Add JavaScript click event and task information to html

--- app/assets/static/javascripts/app.js
+++ app/assets/static/javascripts/app.js
@@ -1,3 +1,9 @@
 $(function() {
   $(".notification").addClass("hidden");
+
+  $("input.todo").click(function() {
+	var this_id = $(this).data("event-id");
+	var this_el = $(".events").find("[data-event-id='" + this_id +"']");
+	console.log(this_el);
+  });
 });
